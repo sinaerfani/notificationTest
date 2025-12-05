2f767f896383d2297a9fdf70c4ec4f99eb3e8409
sinaerfani <sinaerfani66@gmail.com>
2025-12-05 19:03:57 +0400

    finish

---
+++ src/main/java/com/example/notificationtest/NotificationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notificationtest/NotificationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notificationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
